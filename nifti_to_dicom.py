c87a253315ccdbe254ddf0b1081d12fdc1fdb0f0
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-11-21 15:18:31 +0000

    updated date tags to correspond with current datetime

--- nifti_to_dicom.py
+++ nifti_to_dicom.py
@@ -53,6 +53,10 @@ def convertNsave(rescaled_arr, m, b, ipp, pix_spacing, series_id, save_dir, inde
     dateStr = dt.strftime('%Y%m%d')
     dicom_file.InstanceCreationTime = timeStr
     dicom_file.InstanceCreationDate = dateStr
+    dicom_file.StudyDate = dateStr
+    dicom_file.SeriesDate = dateStr
+    dicom_file.AcquisitionDate = dateStr
+    dicom_file.ContentDate = dateStr
 
     #Update SOPInstanceUID (each slice) and SeriesInstanceUID (constant for all slices in series)
     dicom_file.SOPInstanceUID = new_SOP_id
@@ -108,8 +112,8 @@ def nifti_to_dicoms(fpath_nifti_compound, save_dir, fpath_template):
 fpath_template = '/Users/sblackledge/Documents/GENIUSII_exports/RayStation/g02/July_25_1447/CT1.2.826.0.1.3680043.2.1181.1.4.273440548174504.1666686875.0.dcm'
 
 '''START EDIT'''
-save_dir = '/Users/sblackledge/Documents/GENIUSII_exports/compound_dicoms/g02/Aug01'
-fpath_nifti_compound = '/Users/sblackledge/Documents/GENIUSII_exports/nifti_dump/compound_images/g02/Aug01_compound.nii.gz'
+save_dir = '/Users/sblackledge/Documents/GENIUSII_exports/compound_dicoms/g01/June17'
+fpath_nifti_compound = '/Users/sblackledge/Documents/GENIUSII_exports/nifti_dump/compound_images/g01/June17_compound.nii.gz'
 '''END EDIT'''
 
 
